d4844f90d3cd8a31d4d02938d3d3248f6ee970d9
niki843 <niki.tornev96@gmail.com>
2025-01-30 13:22:38 +0200

    Added download_data file to load initial data in as csv and added requests library for fetching added download_data file run in Docker file, added custom exception on fail

---
+++ BE/app/download_data.py
@@ -0,0 +1,31 @@
+import requests
+import os
+from dotenv import load_dotenv
+from app.exceptions.DataLoadException import DataLoadException
+
+load_dotenv()
+
+CSV_URL = os.getenv("JEOPARDY_DATA_URL")
+
+DATA_DIR = "app/data"
+CSV_FILE_PATH = os.path.join(DATA_DIR, "jeopardy_questions.csv")
+
+
+def download_csv():
+    """Used for downloading initial data from Jeopardy"""
+    if os.path.exists(CSV_FILE_PATH) and os.path.getsize(CSV_FILE_PATH) > 0:
+        return
+
+    os.makedirs(DATA_DIR, exist_ok=True)
+
+    response = requests.get(CSV_URL)
+
+    if response.status_code == 200:
+        with open(CSV_FILE_PATH, "w") as csv_data:
+            csv_data.writelines(response.content.decode())
+    else:
+        raise DataLoadException("Could not load initial data")
+
+
+if __name__ == "__main__":
+    download_csv()
